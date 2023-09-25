5d41a90678b1629031c0ca0736cc1eecf1c25143
Walter Vodeb <walter@conexiones.com>
2023-09-25 10:42:18 -0300

    chatbot con Keras

--- keras_chatbot/chatbotWithNLTK.py
+++ keras_chatbot/chatbotWithNLTK.py
@@ -1,22 +1,30 @@
 # https://planetachatbot.com/tutorial-chatbot-usando-nltk-keras/
 
 # pip install tensorflow, keras, pickle, nltk
-import SGD
-from keras.optimizers
+import json  
+import pickle 
+import numpy as np
+
+import nltk 
+from nltk.stem import WordNetLemmatizer
+
 from keras.layers import Dense, Activation, Dropout
 from keras.models import Sequential
-import numpy as np
-import json
-import WordNetLemmatizer
-import nltk from nltk.stem
-import pickleimport
+from keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 randomwords = []
 classes = []
+words=[]
 documents = []
+
+training_sentences = []
+training_labels = []
+labels = []
+responses = []
+
 ignore_words = ['?', '!']
-data_file = open('intents.json').read()
+data_file = open('keras_chatbot/intents.json').read()
 intents = json.loads(data_file)
 
 # 3. Preprocesar datos
@@ -24,22 +32,20 @@ for intent in intents['intents']:
     for pattern in intent['patterns']:
         # tokenize each word
         w = nltk.word_tokenize(pattern)
-        words.extend(w)  # add documents in the corpus
+        words.append(pattern)
+        #words.extend(w)  # add documents in the corpus
         documents.append((w, intent['tag']))   # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-
 # lemmatize, lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower())
-         for w in words
-         if w not in ignore_words]
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
 # documents = combination between patterns and intents
 print(len(documents), "documents")
-classes = intents
+# classes = intents
 print(len(classes), "classes", classes)
 # words = all words, vocabulary
 print(len(words), "unique lemmatized words", words)
@@ -67,20 +73,32 @@ for doc in documents:
             bag.append(1)
         else:
             bag.append(0)
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+        # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
     training.append([bag, output_row])
-    # shuffle our features and turn into np.array
-    random.shuffle(training)
+        # shuffle our features and turn into np.array
+    np.random.shuffle(training)
     training = np.array(training)
     # create train and test lists.
     # X - patterns, Y - intents
-    train_x = list(training[:, 0])
-    train_y = list(training[:, 1])
-    print("Training data created")
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+print("Training data created")
 
 # 5. Construye el modelo
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons  # equal to number of intents to predict output intent with softmax  model = Sequential()  model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))  model.add(Dropout(0.5))  model.add(Dense(64, activation='relu'))  model.add(Dropout(0.5))  model.add(Dense(len(train_y[0]), activation='softmax'))# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model  sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)  model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])#fitting and saving the model   hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)  model.save('chatbot_model.h5', hist)  print("model created")
-
-# 6. Predecir la respuesta (interfaz gráfica de usuario)
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+#  equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print("model created")
