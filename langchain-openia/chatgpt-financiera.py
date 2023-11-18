506e115c5cb3ca1d263ca63201a8173334a79fae
Walter Vodeb <walter@conexiones.com>
2023-11-18 20:54:52 -0300

    chat de financiera

--- langchain-openia/chatgpt-financiera.py
+++ langchain-openia/chatgpt-financiera.py
@@ -37,18 +37,21 @@ llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
 
 
 # Construye un templeta de prompt
-template = """Utiliza todas las piezas del Texto y de la Historia para responder.
- Debes realizar las siguiente acciones:
+template = """ Debes actuar como un agente de atencion a clientes. Debes dirigirte al interlocutor de una manera cordial y atenta, en lo posible por su nombre.
+Utiliza todas las piezas del Texto y de la Historia para responder las preguntas. Si no contestas la pregutna, simplemente di que no lo sabes,no trates de crear una respuesta. Utiliza tres oraciones como maximo.
+ Manten las respuestas concisas.
+ Debes tener en consideracion:
+ 1. Si el interlocutor quiere sacar un prestamo, o desea pagar una deuda, o saber el estado de su cuenta, debes pedirle su nombre y su DNI.
+ 2. Si el interlocutor desea que le transfieras con un agente de ventas, o un supervisor, asegurate de tener el <dni> <nombre> y <apellido>, debes pedirle amablemente su nombre y su DNI y luego finalizar la conversacion.
+ Ademas debes:
  1. Resumir el texto en maximo 1 oracion y etiquetalo como <resumen>.
  2. Si existen nombres de personas separa el nombre de pila y etiquetarlo como <nombre> y el o los apellidos como <apellido>.
  3. Si existe parte del texto numeros de 7 u 8 caracteres etiquetarlo como <dni> y formatearlo como xx.xxx.xxx .
  4. Extraer la intencion en un maximo de 2 palabras y etiquetalo como <intencion>.
- 5. Generar una respuesta con una frase amigable en una etiqueta <frase>.
- 6. Si cononces el nombre de la persona incluye su nombre en la respuesta.
- 7. La resuesta no puede ser mas de 2 oraciones.
- 8. Devoler la respuesta en un json con el siguiente formato:
-
- Texto: {query}
+ 5. En todos los casos, genera una respuesta amigable con una frase amigable en una etiqueta <frase>.
+ 6. Si cononces el nombre de la persona incluye su nombre en la respuesta <frase>.
+ 7. La <frase> no puede ser mas de 2 oraciones.
+ 9. Devoler la respuesta en un json con el siguiente formato, donde <resumen>, <intencion>, <nombre>, <apellido>, <dni>, <frase> son los valores extraidos de los pasos anteriores. En el caso que no se pueda extraer algun valor, se debe devolver un string vacio.
  Resumen: <resumen>
  Intencion: <intencion>
  Nombre: <nombre>
@@ -56,9 +59,11 @@ template = """Utiliza todas las piezas del Texto y de la Historia para responder
  Documento: <dni>
  Respuesta: <frase>
 
-Texto: {query}
-Historia: {chat_history}"""
-QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+ query: {query}
+ chat_history : {chat_history}
+ """
+QA_CHAIN_PROMPT = PromptTemplate.from_template(role="Agente de Ventas", template=template)
 
 chain = LLMChain(llm=llm, prompt=QA_CHAIN_PROMPT)
 
@@ -72,7 +77,7 @@ while True:
     result = chain({"query": query, "chat_history": chat_history})
     print(result['text'])
     dato= json.loads(result['text'])
-    print(dato["Respuesta"])
+    print(dato["frase"])
 
-    chat_history.append((query, dato['Respuesta']))
+    chat_history.append((query, dato['frase']))
     query = None
